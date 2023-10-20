25f34116ab2268c280bf453e625265c35f9164fd
wakidurrahman <wakidur.rahman@4digit.jp>
2023-10-20 18:46:04 +0900

    feat: implement register controller

--- auth/src/controllers/auth-controller.ts
+++ auth/src/controllers/auth-controller.ts
@@ -1,69 +1,107 @@
 import { NextFunction, Request, Response } from 'express';
 import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
-import { getUserByEmail } from '@/db/users-query';
+import { createUser, getUserByEmail } from '@/db/users-query';
 import { BadRequestError } from '@/errors/bad-request-error';
 import { RequestValidationError } from '@/errors/request-validation-error';
-import { createUserService } from '@/services/auth-service';
 import Message from '@/utils/message';
 
 /**
  * register Controller
- * @param req 
- * @param res 
- * @param next 
- * @returns 
+ * @param req
+ * @param res
+ * @param next
+ * @returns
  */
-export const register = 
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // step 1: Check body 
-      const { email, password } = req.body;
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // step 1: Check request  body
+    const { email, password } = req.body;
 
-      if (!email || !password ) {
-        return next(new BadRequestError(`${Message.REQUEST_BODY_IS_EMPTY}`));
+    if (!email || !password) {
+      return next(new BadRequestError(`${Message.REQUEST_BODY_IS_EMPTY}`));
+    }
+
+    // step 2: Handling validation errors
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return next(new RequestValidationError(errors.array()));
+    }
+
+    // step 3: Check Existing User
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return next(
+        new BadRequestError(`${Message.ACCOUNT_WITH_THAT_EMAIL_ALREADY_EXISTS}`)
+      );
+    }
+
+    // step 4: Create new user
+
+    const user = await createUser({
+      email,
+      password,
+    });
+
+    // step 6: Generate JWT
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!,
+      {
+        expiresIn: process.env.JWT_EXP!,
       }
-  
-     // step 2: Handling validation errors 
-      const errors = validationResult(req);
+    );
 
-      if (!errors.isEmpty()) {
-        return next(new RequestValidationError(errors.array()));
-      }
+    // step 7: Store it on session object
+    req.session = {
+      jwt: token
+    }
+    // step 8: Response After user implement
 
-     // step 3: Check Existing User
-      const existingUser = await getUserByEmail(email);
-
-      if (existingUser) {
-        return next(new BadRequestError('Email in use'));
-      }
-      await createUserService({ email, password });
-
-      res.status(201).json({
+    return res
+      .status(201)
+      .json({
         status: 'success',
         message: Message.CREATE_SUCCESSFUL,
-        data: null,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-export const singUpUser = 
-  async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
-    res.status(200).json({
-      status: 'Ok',
-    });
+        data: user,
+      })
+      .end();
+  } catch (error) {
+    console.log(error);
+    return next(new BadRequestError(`${Message.SOME_THING_MISSING}`));
   }
-;
+};
+
+export const singUpUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log(req.body);
+  res.status(200).json({
+    status: 'Ok',
+  });
+};
 /**
  * Fetch Particular user
  * GET /posts
  */
-export const getParticularUser = 
-  async (req: Request, res: Response, next: NextFunction) => {
-    res.status(201).json({
-      status: 'Ok',
-    });
-  };
+export const getParticularUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(201).json({
+    status: 'Ok',
+  });
+};
