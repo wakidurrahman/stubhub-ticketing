dbc95aa1f1a883d196849a80e287702a093b0f70
wakidurrahman <wakidur.rahman@4digit.jp>
2023-10-20 16:22:31 +0900

    auth service auth register route implement

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,51 +1,33 @@
-import { json } from 'body-parser';
-import cors from 'cors';
-import express, { Express, urlencoded } from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-
-const app: Express = express();
-const port = process.env.PORT || 5000;
-app.use(json());
-app.use(urlencoded({ extended: true }));
-
-import { NotFoundError } from './errors/not-found-error';
-import { errorHandler } from './middlewares/error-middleware';
-import authRoutes from './routes/auth-routes';
-
-{/* ↓↓↓ CORS Settings */}
-app.use(cors({
-  credentials: true,
-}));
-
-
-{/* ↓↓↓ Route Handler */}
-app.use(authRoutes);
-
-app.all('*', async (req, res) => {
-  console.log("Incomming ")
-  console.log(req.body)
-  console.log(req.url)
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
-{/* ↑↑↑ Route Handler */}
+import app from './app';
+const port = process.env.PORT;
 
 const start = async () => {
   try {
-    {/* ↓↓↓ Mongoose Connect For Local Kubernetes*/}
-     await mongoose.connect('mongodb://auth-mongo-clusterip-service:27017/auth');
-    {/* ↓↓↓ Mongoose Connect For Local Environment*/}
-    // await mongoose.connect('mongodb://localhost:27017/stubhub-ticketing-auth');
+    {
+      /* ↓↓↓ Mongoose Connect For Local Kubernetes*/
+    }
+    // await mongoose.connect('mongodb://auth-mongo-clusterip-service:27017/auth');
+    
+    {
+      /* ↓↓↓ Mongoose Connect For Local Environment*/
+    }
+    if (!process.env.JWT_KEY) {
+      throw new Error('JWT_KEY must be defined');
+    }
+    if (!process.env.MONGO_URI_LOCAL) {
+      throw new Error('MONGO_URI must be defined');
+    }
+    await mongoose.connect(process.env.MONGO_URI_LOCAL);
     console.log('Connected to MongoDb successful');
   } catch (error) {
     console.error(error);
   }
 
- {/* ↓↓↓ Server start */}
+  {
+    /* ↓↓↓ Server start */
+  }
   app.listen(port, () => {
-    console.log("first workding")
     console.log(`Auth app listening on port ${port}`);
   });
 };
