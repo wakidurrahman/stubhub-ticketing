e4e1f071b758af5b5835e2f6f282b39d810504d4
wakidurrahman <wakidur.rahman@4digit.jp>
2023-10-30 19:04:33 +0900

    update code

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
